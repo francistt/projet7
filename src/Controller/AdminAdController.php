d5ad3f0941d95869c339e62f3eddca25d3897d16
Francis <francist@hotmail.com>
2020-12-19 14:54:07 +0100

    ajout administration

---
+++ src/Controller/AdminAdController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Form\AdType;
+use App\Service\Pagination;
+use App\Service\StatsService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminAdController extends AbstractController
+{
+    /**
+     * @Route("/admin/ads/{page<\d+>?1}", name="admin_ads_index")
+     */
+    public function index($page, Pagination $pagination, StatsService $statsService)
+    {
+        $stats = $statsService->getStats();
+
+        $pagination->setEntityClass(Ad::class)
+                   ->setLimit(10)
+                   ->setPage($page);
+
+        return $this->render('admin/ad/index.html.twig', [
+           'pagination' => $pagination,
+           'stats' => $stats
+        ]);
+    }
+
+    /**
+     * Permet d'afficher le formulaire d'édition
+     * 
+     * @Route("/admin/ads/{id}/edit", name="admin_ads_edit")
+     *
+     * @param Ad $ad
+     * @return Response
+     */
+    public function edit(Ad $ad, Request $request, EntityManagerInterface $manager) {
+        $form = $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getName()}</strong> a bien été enregistrée !"
+            );
+        }
+
+        return $this->render('admin/ad/edit.html.twig', [
+            'ad' => $ad,
+            'form' => $form->createView()
+            ]);
+    }
+
+    /**
+     * Permet de supprimer une annonce
+     *
+     * @Route("/admin/ads/{id}/delete", name="admin_ads_delete")
+     * 
+     * @param Ad $ad
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function delete(Ad $ad, EntityManagerInterface $manager) {
+        $manager->remove($ad);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            "L'annonce <strong>{$ad->getName()}</strong> a bien été suprimée !"
+        );
+
+        return $this->redirectToRoute('admin_ads_index');
+    }
+}
